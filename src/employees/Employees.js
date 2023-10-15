a5d622abde0902256ec613db3f857bedb42cb8c8
Megan Tan <meganytan@gmail.com>
2023-10-15 03:41:13 -0500

    Initial commit

---
+++ src/employees/Employees.js
@@ -0,0 +1,20 @@
+import './Employees.css';
+
+function Login() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Employee ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone Number</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+    </table>
+  );
+}
+
+export default Login;
